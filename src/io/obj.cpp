c36c20f2ded35ae051dd98c2b31433f0ee6c5d7f
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2020-05-10 16:49:19 -0700

    start OBJ saver

--- src/io/obj.cpp
+++ src/io/obj.cpp
@@ -1,14 +1,31 @@
 #include "hilma/io/obj.h"
 
+#include <stdio.h>
+#include <iostream>
+#include <limits>
+#include <iomanip>
+#include <fstream>
+#include <cstdio>
+#include <cassert>
+
+#ifndef HILMA_LINE_MAX
+#define HILMA_LINE_MAX 2048
+#endif
+
 namespace hilma {
 
-namespace io {
-
 bool saveObj( const std::string& _filename, const Mesh& _mesh ) {
-    return true;
-}
+    FILE * obj_file = fopen(_filename.c_str(), "w");
+    if (NULL==obj_file) {
+        printf("IOError: %s could not be opened for writing...", _filename.c_str());
+        return false;
+    }
 
-}
+    // https://github.com/libigl/libigl/blob/master/include/igl/writeOBJ.cpp
+
+    return true;
+};
+
 
 }
 
